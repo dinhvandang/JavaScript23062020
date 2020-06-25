98fe24bda7a264f708518797d204942f71ccd526
dinhvandang <dinhvandang.dev@gmail.com>
2020-06-25 20:56:51 +0700

    object and array

--- buoi1.js
+++ buoi1.js
@@ -12,4 +12,22 @@
 //th1 : khai bao 1 bien khong gan gia tri = undefined
 // var a
 // console.log(a)
+// th2 : truy van toi key(ob) khong ton tai thi la undefined
+// const teo={
+//     name : "Nguyen Van Teo", age: 10
+// }
+
+// console.log(teo.address)
+
+
 //3 :  object
+// const teo={
+//     name : "Nguyen Van Teo", age: 10
+// }
+
+// console.log(teo.address)
+
+//4 : array
+ const arrayNames = ["Teo","Ti","Tun"]
+ arrayNames[0] = "Tuan"
+console.log(arrayNames[0])
