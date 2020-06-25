7fa46b5ba600504e90c14b64254c17e4d3f8963d
dinhvandang <dinhvandang.dev@gmail.com>
2020-06-25 20:27:26 +0700

    khai bao bien va kieu du lieu

---
+++ buoi1.js
@@ -0,0 +1,15 @@
+//1 : khai bao bien
+// var a=5
+// const b=6
+// a=10
+// b=12
+// console.log(b)
+
+//2 : kieu du lieu
+// null , undefined
+// var a=null
+// console.log(a)
+//th1 : khai bao 1 bien khong gan gia tri = undefined
+// var a
+// console.log(a)
+//3 :  object
