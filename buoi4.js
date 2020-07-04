c0df450752ee5f1c044db1cc01831ef4893db945
dinhvandang <dinhvandang.dev@gmail.com>
2020-07-04 20:18:49 +0700

    cach su dung bien

---
+++ buoi4.js
@@ -0,0 +1,22 @@
+// 22 : toan tu 3 ngoi
+const a =5
+const b = 6 
+    // if (a > b){
+    //     console.log("A lon hon B")
+    // }else{
+    //     console.log("A be hon hoac bang B")
+    // }
+    // console.log(a > b ? "A lon hon B" : ("A be hon hoac bang B") ) // neu a lon hon b thi dau ? la chay a>b , con dau : la hoac a<=b
+
+// 23 : function scope
+    // var a = 5 
+    // function tinhtong(){
+    //     var a = 7
+    //     var b = 6
+    //     return a + b;
+    // }
+    // console.log(tinhtong())
+    
+// 24 : cach su dung ks bien
+
+
